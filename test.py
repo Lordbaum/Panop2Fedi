28a6fbc87ac745bcc94ebe84f0cbf0446c5cf513
Lordbaum <lordbaum@disroot.org>
2024-04-26 10:48:01 +0200

    rm

--- test.py
+++
@@ -1,4 +0,0 @@
-from mastodon import Mastodon
-mastodon  =  Mastodon(access_token = "x5AAsQTsY-l67NPcyiME4Z-2S2hsypApugG4lvYIC4c", api_base_url="http://mastodon.local")
-#mastodon.log_in( "admin@mastodon.local", "mastodonadmin", "ZmcIdVl8SwwjwAWGc5hDrzc1jx7oXUdruVm0he6nEqY" )
-mastodon.toot("hi")
